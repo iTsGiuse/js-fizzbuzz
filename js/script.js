dd55df64082965c39ce5146a84dfb8554c994bf0
iTsGiuse <giuse.amato@icloud.com>
2024-03-07 19:05:35 +0100

    fix: fix

--- js/script.js
+++ js/script.js
@@ -8,36 +8,44 @@ for (let i = 1; i <= 100; i++) {
         const fizzBuzz = 'fizzbuzz';
         console.log(fizzBuzz);
 
+        /* AGGIUNGI UNA CARD CON LA SUA CLASSE, PROPRIETA' E VARIABILE NELL'HTML SOTTO AL DIV-CONTAINER */
         let card = document.querySelector("div.container");
-        card.innerHTML += `<card class"quadrato" style="border: 1px solid black; width: 80px; height: 80px; background-color: #f0466f; text-align: center; padding-top: 30px;"> ${fizzBuzz} </card>`;
+        card.innerHTML += `<div class"quadrato" style="border: 1px solid black; width: 80px; height: 80px; background-color: #f0466f; text-align: center; padding-top: 30px;"> ${fizzBuzz} </div>`;
         
     /* STAMPA I MULTIPLI DI 3 */
     } else if (i % 3 === 0) {
         const fizz = 'fizz';
         console.log(fizz);
 
+        /* AGGIUNGI UNA CARD CON LA SUA CLASSE, PROPRIETA' E VARIABILE NELL'HTML SOTTO AL DIV-CONTAINER */
         let card = document.querySelector("div.container");
-        card.innerHTML += `<card class"quadrato" style="border: 1px solid black; width:80px; height: 80px; background-color: #0cd6a1; text-align: center; padding-top: 30px;"> ${fizz} </card>`;
+        card.innerHTML += `<div class"quadrato" style="border: 1px solid black; width:80px; height: 80px; background-color: #0cd6a1; text-align: center; padding-top: 30px;"> ${fizz} </div>`;
     
     /* STAMPA I MULTIPLI DI 5 */
     } else if (i % 5 === 0) {
         const buzz = "buzz";
         console.log(buzz);
 
+        /* AGGIUNGI UNA CARD CON LA SUA CLASSE, PROPRIETA' E VARIABILE NELL'HTML SOTTO AL DIV-CONTAINER */
         let card = document.querySelector("div.container");
-        card.innerHTML += `<card class"quadrato" style="border: 1px solid black; width:80px; height: 80px; background-color: #ffd166; text-align: center; padding-top: 30px;"> ${buzz} </card>`;
+        card.innerHTML += `<div class"quadrato" style="border: 1px solid black; width:80px; height: 80px; background-color: #ffd166; text-align: center; padding-top: 30px;"> ${buzz} </div>`;
 
     } else {
         console.log(i);
 
-        let card = document.querySelector("div.container");
-        /* card.innerHTML += `<card class"quadrato" style="border: 1px solid black; width:80px; height: 80px; background-color: #1389b2; text-align: center; padding-top: 30px;"> ${i} </card>`; */
+        /* AGGIUNGI UNA CARD NELL'HTML SOTTO AL DIV-CONTAINER */
+        let card = document.querySelector("div.container"); 
+        card.innerHTML += `<div class="quadrato" style="border: 1px solid black; width:80px; height: 80px; background-color: #1389b2; text-align: center; padding-top: 30px;"> ${i} </div>`; 
+        
+        /* INSERIMENTO VARIABILE COME TESTO DELLA CARD */
+   /*      const card = document.createElement("div");
         card.innerHTML = i;
-        card.nameClass.add('quadrato');
-        card.style.border= "10px solid black";
-        card.append(append);
+        card.classList.add('quadrato');
+        card.style.border= "10px solid white";
+        card.style.width= "80px"; 
+        card.style.height= "80px";
+        card.append('card');   */
     }
-
 
 
 }
